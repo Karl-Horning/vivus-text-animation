7e4a5e65666b1c25cbf7e05498d1f98b7e580c6a
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-02-09 00:45:07 +0000

    Updated Vivus and added SVG and script

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+new Vivus("helloWorld", { duration: 200 }, function () {
+    $("#helloOutline").attr("style", "fill:white");
+    $("#worldOutline").attr("style", "fill:white");
+    $("#helloBg").attr("style", "fill:rgb(45, 41, 45)");
+    $("#worldBg").attr("style", "fill:rgb(45, 41, 45)");
+});
