820f5a4f29b67ed08481ca13a1e292627af6b986
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-02-09 08:32:13 +0000

    Added function to fade in fill colours

--- src/js/script.js
+++ src/js/script.js
@@ -10,17 +10,56 @@ document.addEventListener("DOMContentLoaded", () => {
     const helloBg = document.getElementById("helloBg");
     const worldBg = document.getElementById("worldBg");
 
+    // Constants for colours and timings
+    const transparentBlack = "rgba(0, 0, 0, 0)";
+    const fillBlack = "rgba(0, 0, 0, 1)";
+
     /**
      * Callback function to execute when the animation is complete.
-     * Changes the fill colors of SVG elements.
+     * Changes the fill colors of SVG elements with a fade effect.
      */
     const onComplete = () => {
         console.log("Animation complete");
         // Additional code to execute when the animation is complete
-        helloOutline.style.fill = "white";
-        worldOutline.style.fill = "white";
-        helloBg.style.fill = "rgb(45, 41, 45)";
-        worldBg.style.fill = "rgb(45, 41, 45)";
+        // Fading in different SVG elements with specified timing and colours
+        fadeFill({ element: helloOutline, transitionTime: 1.2 });
+        fadeFill({ element: worldOutline });
+        fadeFill({
+            element: helloBg,
+            transparentColour: transparentBlack,
+            fillColour: fillBlack,
+            transitionTime: 2,
+        });
+        fadeFill({
+            element: worldBg,
+            transparentColour: transparentBlack,
+            fillColour: fillBlack,
+            transitionTime: 2.2,
+        });
+    };
+
+    /**
+     * Applies a fade effect to the fill color of an SVG element.
+     * @param {Object} options - Options for the fade effect.
+     * @param {HTMLElement} options.element - The SVG element to apply the fade effect.
+     * @param {string} [options.transparentColour="rgba(255, 255, 255, 0)"] - The transparent colour for the fade effect.
+     * @param {string} [options.fillColour="rgba(255, 255, 255, 1)"] - The fill colour after the fade effect.
+     * @param {number} [options.transitionTime=1] - The duration of the fade effect transition in seconds.
+     */
+    const fadeFill = ({
+        element,
+        transparentColour = "rgba(255, 255, 255, 0)",
+        fillColour = "rgba(255, 255, 255, 1)",
+        transitionTime = 1,
+    }) => {
+        // Set initial opacity to 0
+        element.style.fill = transparentColour;
+        // Apply transition for smooth fade
+        element.style.transition = `fill ${transitionTime}s cubic-bezier(0.79, 0.33, 0.14, 0.53)`;
+        // Set final opacity to 1 after a short delay
+        setTimeout(() => {
+            element.style.fill = fillColour;
+        }, 100);
     };
 
     // Initialize Vivus SVG animation
